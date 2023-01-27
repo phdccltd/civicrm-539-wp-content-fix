282b749ce944cac877f03b0b69f36a2a23638fab
Chris Cant <chris@phdcc.com>
2023-01-27 20:12:44 +0000

    Only call basepage_render if basepage_markup set

--- civicrm-content-fix.php
+++ civicrm-content-fix.php
@@ -6,7 +6,7 @@ Plugin URI: https://github.com/phdccltd/civicrm-539-wp-content-fix
 GitHub Plugin URI: phdccltd/civicrm-539-wp-content-fix
 Description: Plugin to fix CiviCRM 5.39+ WordPress content issue
 Author: Chris Cant, PHDCC
-Version: 1.2
+Version: 1.3
 Author URI: https://www.phdcc.com/
 License: GPL2
 */
@@ -28,9 +28,11 @@ function civicrm_539_wp_content_fix_init($wp) {
 function civicrm_539_wp_content_fix_basepage_render($html) {
 
   try{
-    $newhtml = civi_wp()->basepage->basepage_render($html);
-    if( strlen($newhtml)>0){
-      $html = $newhtml;
+    if( isset(civi_wp()->basepage->basepage_markup)){ // 1.3
+      $newhtml = civi_wp()->basepage->basepage_render($html);
+      if( strlen($newhtml)>0){
+        $html = $newhtml;
+      }
     }
   } catch(Exception $e){ 
   }
